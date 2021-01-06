1481f87e15107a5a1b1bb5b7118330dabc8c3af4
EC2 Default User <ec2-user@ip-172-31-6-204.us-east-2.compute.internal>
2021-01-06 12:07:43 +0000

    pictgram

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'pages/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'pages/index'
+  root 'pages#index'
+  get 'pages/help'
+  
+  resources :users
+end
