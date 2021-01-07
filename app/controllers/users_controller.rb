07ece6a69a5641bd4607e4f1fa1dd1e3beb31d79
shion <shion-1216@yahoo.co.jp>
2021-01-07 12:22:11 +0000

    2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,17 @@ class UsersController < ApplicationController
   
   def create
     #binding.pry
-    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, success: '登録が完了しました'
     else
-      render :new
+      flash.now[:danger] = "登録に失敗しました"
+      render :new 
     end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
