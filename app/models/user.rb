3131198cff078656a39ce0391f0e842c9803a17b
shion <shion-1216@yahoo.co.jp>
2021-01-09 07:38:46 +0000

    ログインページ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   #名前空白・長さ
-  validates :name, presence: true, lenght:{maximum: 15}
+  validates :name, presence: true, length:{maximum: 15}
   
   #メアド空白・~@~.~ になってるはず？
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   
   #パスワード空白・文字数・ローマ字数字
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
-  validates :password, presence: true, lenght: {in: 8..32}, format: {with: VALID_PASSWORD_REGEX}
+  validates :password, presence: true, length: {in: 8..32}, format: {with: VALID_PASSWORD_REGEX}
   
   
   has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'pages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-
-Rails.application.routes.draw do
-  get 'users/new'
-  get 'pages/index'
+  get 'sessions/new'
+  
+  #get 'users/new'
+  resources :users
+  
+  #get 'pages/index'
   root 'pages#index'
   get 'pages/help'
   
-  resources :users
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
 end
