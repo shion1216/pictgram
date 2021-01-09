84f399123317c3bd1fe2e3305e8da17c841acf2c
shion <shion-1216@yahoo.co.jp>
2021-01-09 06:07:40 +0000

    バリデーションの課題

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  #名前空白・長さ
+  validates :name, presence: true, lenght:{maximum: 15}
+  
+  #メアド空白・~@~.~ になってるはず？
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+  
+  #パスワード空白・文字数・ローマ字数字
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
+  validates :password, presence: true, lenght: {in: 8..32}, format: {with: VALID_PASSWORD_REGEX}
+  
   
   has_secure_password
 end
